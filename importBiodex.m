f653d93678b8de8fb00a4b945902eb6ed9ad72fa
megarcia12 <117217008+megarcia12@users.noreply.github.com>
2022-11-02 09:49:13 -0400

    Update importBiodex.m

--- importBiodex.m
+++ importBiodex.m
@@ -21,82 +21,81 @@ function [data,metadata] = importBiodex(filename, dataLines)
 
 % If dataLines is not specified, define defaults
 if nargin < 2
-    dataLines = [53, Inf];
+    dataLines = [53, Inf] ;
 end
 
 %% Set up the Import Options and import the data
-opts = delimitedTextImportOptions("NumVariables", 8, "Encoding", "UTF-8");
+opts = delimitedTextImportOptions("NumVariables", 8, "Encoding", "UTF-8") ;
 
 % Specify range and delimiter
-opts.DataLines = dataLines;
-opts.Delimiter = " ";
+opts.DataLines = dataLines ;
+opts.Delimiter = " " ;
 
 % Specify column names and types
-opts.VariableNames = ["Set", "Rep", "mSec", "TORQUE", "POSITION", "POS_ANAT", "VELOCITY", "Var8"];
-opts.SelectedVariableNames = ["Set", "Rep", "mSec", "TORQUE", "POSITION", "POS_ANAT", "VELOCITY"];
-opts.VariableTypes = ["double", "double", "double", "double", "double", "double", "double", "string"];
+opts.VariableNames = ["Set", "Rep", "mSec", "TORQUE", "POSITION", "POS_ANAT", "VELOCITY", "Var8"] ;
+opts.SelectedVariableNames = ["Set", "Rep", "mSec", "TORQUE", "POSITION", "POS_ANAT", "VELOCITY"] ;
+opts.VariableTypes = ["double", "double", "double", "double", "double", "double", "double", "string"] ;
 
 % Specify file level properties
-opts.ExtraColumnsRule = "ignore";
-opts.EmptyLineRule = "read";
-opts.ConsecutiveDelimitersRule = "join";
-opts.LeadingDelimitersRule = "ignore";
+opts.ExtraColumnsRule = "ignore" ;
+opts.EmptyLineRule = "read" ;
+opts.ConsecutiveDelimitersRule = "join" ;
+opts.LeadingDelimitersRule = "ignore" ;
 
 % Specify variable properties
-opts = setvaropts(opts, "Var8", "WhitespaceRule", "preserve");
-opts = setvaropts(opts, "Var8", "EmptyFieldRule", "auto");
-opts = setvaropts(opts, "Set", "TrimNonNumeric", true);
-opts = setvaropts(opts, "Set", "ThousandsSeparator", ",");
+opts = setvaropts(opts, "Var8", "WhitespaceRule", "preserve") ;
+opts = setvaropts(opts, "Var8", "EmptyFieldRule", "auto") ;
+opts = setvaropts(opts, "Set", "TrimNonNumeric", true) ;
+opts = setvaropts(opts, "Set", "ThousandsSeparator", ",") ;
 
 %% Import the data
-tbl = readtable(filename, opts);
-ind.rep1 = ((tbl.Rep== 1)) ;
-ind.rep2 = ((tbl.Rep== 2)) ;
-angles = angleImport(filename);
+tbl = readtable(filename, opts) ;
+ind.rep1 = ((tbl.Rep== 1)) ; % Finds Rep 1
+ind.rep2 = ((tbl.Rep== 2)) ; % Finds Rep 2
+angles = angleImport(filename) ; % Imports the angles for the joint
 for na = 1:length(angles)
-    ind.(['angle',num2str(angles(1,na))]) = (tbl.Set==na) ;
-    cangle = (['angle',num2str(angles(1,na))]) ;
+    ind.(['a',num2str(angles(1,na))]) = (tbl.Set==na) ;
+    cangle = (['a',num2str(angles(1,na))]) ; % Sets current angle
     
     for nReps = 1:2
         crep = ['rep',num2str(nReps)] ;
         itrial = (ind.(cangle)) & (ind.(crep)) ;
-        data.(cangle).(crep) = [tbl.mSec(itrial),tbl.TORQUE(itrial)];
+        data.(cangle).(crep) = [tbl.mSec(itrial),tbl.TORQUE(itrial)] ; % Saves data for time and torque
     end
     
 end
 
 %% Sets up Meta Data Analysis
-info = trialDataImport(filename);
-metadata.subject = string(info(1));
-metadata.joint = string(info(19));
-metadata.dof = string(info(20));
-metadata.bdirection  = string(info(22));
+info = trialDataImport(filename) ;
+metadata.subject = string(info(1)) ; % Subject ID
+metadata.joint = string(info(19)) ; % Joint
+metadata.dof = string(info(20)) ; % Biodex designation
+metadata.bdirection  = string(info(22) ); % Direction of movement
 
 if contains(metadata.joint,'Ankle')==1
-    if contains(metadata.dof, 'Plantar') && contains(metadata.bdirection, 'TOWARDS')
-        metadata.match = 'dors';
+    if contains(metadata.dof, 'Plantar') && contains(metadata.bdirection, 'TOWARDS') 
+        metadata.match = 'dors' ; % Assigns dorsiflexion based on biodex direction
     else 
-        metadata.match = 'plnt';
+        metadata.match = 'plnt' ; % Assigns plantar flexion based on biodex direction
     end
     
 elseif contains(metadata.joint,'Knee')==1
     if contains(metadata.dof, 'Extension') && contains(metadata.bdirection, 'TOWARDS')
-        metadata.match = 'flx';
+        metadata.match = 'flx' ; % Assigns flexion based on biodex direction
     else 
-        metadata.match = 'ext';
+        metadata.match = 'ext' ; % Assigns extension based on biodex direction
     end
     
 elseif  contains(metadata.joint,'Hip')==1
     if contains(metadata.dof, 'Extension') && contains(metadata.bdirection, 'TOWARDS')
-        metadata.match = 'ext';
+        metadata.match = 'ext' ; % Assigns extension based on biodex direction
     elseif contains(metadata.dof, 'Extension') && contains(metadata.bdirection, 'AWAY')
-        metadata.match = 'flx';
+        metadata.match = 'flx' ; % Assigns flexion based on biodex direction
     elseif contains(metadata.dof, 'Abduction') && contains(metadata.bdirection, 'TOWARDS')
-        metadata.match = 'ad';
+        metadata.match = 'ad' ; % Assigns adduction based on biodex direction
     else 
-        metadata.match = 'ab';
+        metadata.match = 'ab' ; % Assigns abduction based on biodex direction
     end
 end
-
 
 end
